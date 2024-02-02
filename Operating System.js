3daec7c83deeb5dbd6334e6f04d21b68c986e3da
Jahred-Mario <jahredbaccay97@gmail.com>
2024-02-02 19:22:45 +0800

    first commit

---
+++ Operating System.js
@@ -0,0 +1,79 @@
+function Wrong1() {
+
+}
+
+function Correct1() {
+
+}
+
+function Wrong2() {
+
+}
+
+function Correct2() {
+
+}
+
+function Wrong3() {
+
+}
+
+function Correct3() {
+
+}
+
+function Wrong4() {
+
+}
+
+function Correct4() {
+
+}
+
+function Wrong5() {
+
+}
+
+function Correct5() {
+
+}
+
+function Wrong6() {
+
+}
+
+function Correct6() {
+
+}
+
+function Wrong7() {
+
+}
+
+function Correct7() {
+
+}
+
+function Wrong8() {
+
+}
+
+function Correct8() {
+
+}
+
+function Wrong9() {
+
+}
+
+function Correct9() {
+
+}
+
+function Wrong10() {
+
+}
+
+function Correct10() {
+
+}
